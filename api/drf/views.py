b75dcad2eb0a3c23bbee9f1906cc0db5d49c3a51
filip <filip@email.com>
2024-12-12 18:34:51 +0100

    refac: Removed WS, cleaned and upgraded

--- api/drf/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import JsonResponse
-
-
-def error_404_view(request):
-    return JsonResponse({
-        "detail": "This resource does not exist."
-    }, status=404)
--- backend/hosts.py
+++
@@ -1,7 +0,0 @@
-from django_hosts import patterns, host
-
-host_patterns = patterns(
-    '',
-    host(r'api', 'api.drf.urls', name='api'),
-    host(r'ws', 'api.ws.routing', name='ws'),
-)
