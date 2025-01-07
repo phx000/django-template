c29e92cd32f2e7498a14a3194f4c4d0436bfe8f0
filip@work <filip@work.com>
2025-01-07 16:34:23 +0100

    fix: workflow

--- .github/configmap_to_env_file.py
+++ .github/configmap_to_env_file.py
@@ -1,13 +1,13 @@
 import json
 
 def create_env_file_from_configmap():
-    with open("k8s/_configmap_output.json") as file:
+    with open(".github/_configmap_output.json") as file:
         json_string="\n".join(file.read().splitlines()[2:])
         configmap=json.loads(json_string)
 
     env_string="\n".join([f"{key}={value}" for key, value in configmap["data"].items()])
 
-    with open("k8s/_configmap.env", "w") as file:
+    with open(".github/_configmap.env", "w") as file:
         file.write(env_string)
 
 if __name__ == "__main__":
